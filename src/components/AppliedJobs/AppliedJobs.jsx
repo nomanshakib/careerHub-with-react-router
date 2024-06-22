8b79a7b39efaaf98339c0c0e9694343d2f47f9ac
nomanshakib <nomanshakib.official@gmail.com>
2024-06-22 12:43:53 +0600

    Display applied jobs from storedId

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,9 +1,48 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getStoredJobApplication } from "../../utility/localstorage";
 
 
 const AppliedJobs = () => {
+    const jobs = useLoaderData();
+
+    const [appliedJobs, setAppliedJobs] = useState([]);
+
+    useEffect(() => {
+        const storedJobIds = getStoredJobApplication();
+        if (jobs.length > 0) {
+            /* const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id)); */
+
+            const jobsApplied = [];
+            for (const id of storedJobIds) {
+                const job = jobs.find(job => job.id === id);
+                if (job) {
+                    jobsApplied.push(job)
+                }
+            }
+            
+            setAppliedJobs(jobsApplied);
+            /* console.log(jobs, storedJobIds, jobsApplied); */
+        }
+    }, [])
     return (
         <div>
-            <h1>AppliedJobs</h1>
+            <h1 className="text-2xl">AppliedJobs : {appliedJobs.length}</h1>
+            <details className="dropdown">
+                <summary className="m-1 btn">open or close</summary>
+                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    <li><a>All</a></li>
+                    <li><a>Remote</a></li>
+                    <li><a>On Site</a></li>
+                </ul>
+            </details>
+            <ul>
+                {
+                    appliedJobs.map(job => <li key={job.id}>
+                        <span>{job.job_title} - {job.company_name} : { job.remote_or_onsite}</span>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
